6db94c80ee3f39479062167ba49df5db5d780cc5
frankzhang2022 <frank.zhang@pixelforce.com.au>
2023-10-25 12:00:51 +1000

    1. use https://www.tailwindtoolbox.com/templates/landing-page as template 2. separate template into nav, head and footer 3. in nav, add auth, add profile edit 4. in navigation-menu.blade.php: add category tag and post TAB

--- resources/views/components/partials/head.blade.php
+++ resources/views/components/partials/head.blade.php
@@ -18,6 +18,8 @@
 <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
 <!-- Scripts -->
+<script src="{{ asset('js/main.js') }}" defer></script>
+<script src="{{ asset('js/drop-down.js') }}" defer></script>
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 <title>@yield('title', 'Blog')</title>
